3bdf75bb6af4b3b1d54b2bb9627ac33dfbf51e17
esHathck05 <46973649+esHathck05@users.noreply.github.com>
2019-02-27 13:55:27 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,14 +4,18 @@
 
 #Assignment: FizzBuzz
 
-for i in range(1, 101):
-    if i in range(0, 101, 3) and i in range(0, 101, 5):
+number = int(input("How many numbers shall we print? ")) + 1
+fizznumber = int(input("For multiples of what number shall we print 'Fizz'? "))
+buzznumber = int(input("For multiples of what number shall we print 'Buzz'? "))
+
+for i in range(1, number):
+    if i in range(0, number, fizznumber) and i in range(0, number, buzznumber):
         print("fizzbuzz")
     else:
-        if i in range(0, 101, 3):
+        if i in range(0, number, fizznumber):
             print("fizz")
         else:
-            if i in range(0, 101, 5):
+            if i in range(0, number, buzznumber):
                 print("buzz")
             else:
                     print(i)
